cfc71c942af7d270fc88c50215ae2c7441a6ea35
Nathan Faucett <nathanfaucett@gmail.com>
2015-06-01 10:47:09 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -12,27 +12,43 @@ module.exports = Phys2DPlugin;
 
 
 function Phys2DPlugin() {
+    var _this = this;
 
     Plugin.call(this);
 
     this.space = new Space();
+
     this.update = Phys2DPlugin_createUpdate(this);
+    this.onAddPhys2DRigidBody = function onAddPhys2DRigidBody(component) {
+        _this.space.add(component.body);
+    };
+    this.onRemovePhys2DRigidBody = function onRemovePhys2DRigidBody(component) {
+        _this.space.remove(component.body);
+    };
 }
 Plugin.extend(Phys2DPlugin, "Phys2DPlugin");
 Phys2DPluginPrototype = Phys2DPlugin.prototype;
 
 Phys2DPluginPrototype.clear = function clear(emitEvent) {
+    var scene = this.scene;
 
     PluginPrototype.clear.call(this, emitEvent);
 
     this.space.clear();
 
+    scene.off("addPhys2DRigidBody", this.onAddPhys2DRigidBody);
+    scene.off("removePhys2DRigidBody", this.onRemovePhys2DRigidBody);
+
     return this;
 };
 
 Phys2DPluginPrototype.init = function init() {
+    var scene = this.scene;
 
     PluginPrototype.init.call(this);
+
+    scene.on("addPhys2DRigidBody", this.onAddPhys2DRigidBody);
+    scene.on("removePhys2DRigidBody", this.onRemovePhys2DRigidBody);
 
     return this;
 };
@@ -58,7 +74,7 @@ function Phys2DPlugin_createUpdate(_this) {
         accumulator += delta;
 
         while (accumulator >= step) {
-            space.update(step);
+            space.step(step);
             accumulator -= step;
         }
 
